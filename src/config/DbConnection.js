6b694085c8ee2242d8a306eb43dcf94affac0915
IzaanArain <izaansaquib@gmail.com>
2023-07-17 18:38:57 +0500

    vercel deployment

--- src/config/DbConnection.js
+++ src/config/DbConnection.js
@@ -1,8 +1,10 @@
 const mongoose=require("mongoose");
 
+const CONNECTION_STRING='mongodb+srv://izaansaquib:izaan1998@procductcluster.5bblmrh.mongodb.net/ProductDatabse?retryWrites=true&w=majority'
 const connectDB=async()=>{
     try{
-        const conn=await mongoose.connect(process.env.CONNECTION_STRING)
+        // const conn=await mongoose.connect(process.env.CONNECTION_STRING)
+        const conn=await mongoose.connect(CONNECTION_STRING);
         console.log(`database connected at : ${conn.connection.host} ${conn.connection.name}`.cyan.underline)
 
     }catch(error){
